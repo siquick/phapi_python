a722b86587a33f2f93a4e20516a404efea84a2f1
Si Quick <siquick@gmail.com>
2017-11-21 17:32:06 +1100

    set a global base URL for the API

--- app/phapi.py
+++ app/phapi.py
@@ -57,14 +57,14 @@ class Publisher():
 
 	#payload = TBC
 	def create_publisher(application_key,user_api_key,payload):
-		endpoint = 'https://api.performancehorizon.com/user/publisher'
+		endpoint = base_url + '/user/publisher'
 		output = api_call_post(self.application_key,self.user_api_key,endpoint,payload)
 		return output
 
 
 	#payload = '{'campaign_id':campaign_id}'
 	def join_campaign(application_key,user_api_key,payload,publisher_id,campaign_status):
-		endpoint = 'https://api.performancehorizon.com/user/publisher/%s/campaign/%s' % (publisher_id,campaign_status)
+		endpoint = base_url + '/user/publisher/%s/campaign/%s' % (publisher_id,campaign_status)
 		output = api_call_post(self.application_key,self.user_api_key,endpoint,payload)
 		return output
 
@@ -78,7 +78,7 @@ class Advertiser():
 
 	#payload = {'account_name': account_name,'company_name':company_name,'address1':address1,'postcode':postcode,'country':country,'phone':phone,'signup_ip':signup_ip}
 	def create_advertiser(self, payload):
-		endpoint = 'https://api.performancehorizon.com/user/advertiser'
+		endpoint = base_url + '/user/advertiser'
 		output = api_call_post(self.application_key,self.user_api_key,endpoint,payload)
 		return output
 
@@ -91,25 +91,25 @@ class Advertiser():
 
 	#payload = {'auto_approval':'y','title':campaign_title,'advertiser_id':advertiser_id,'destination_url':destination_url,'default_commission_value':'0'}
 	def create_campaign(self, payload):
-		endpoint = 'https://api.performancehorizon.com/campaign'
+		endpoint = base_url + '/campaign'
 		output = api_call_post(self.application_key,self.user_api_key,endpoint,payload)
 		return output
 
 	#payload = {'auto_approval':'y','title':campaign_title,'advertiser_id':advertiser_id,'destination_url':destination_url,'default_commission_value':'0'}
 	def update_campaign(self, payload,campaign_id):
-		endpoint = 'https://api.performancehorizon.com/campaign/%s' % (campaign_id)
+		endpoint = base_url + '/campaign/%s' % (campaign_id)
 		output = api_call_put(self.application_key,self.user_api_key,endpoint,payload)
 		return output
 
 	#payload = '{'campaign_id':campaign_id,'action':'delete','retire_reason':'add retire reasons here'}'
 	def delete_campaign(self, payload,campaign_id):
-		endpoint = 'https://api.performancehorizon.com/campaign/%s' % (campaign_id)
+		endpoint = base_url + '/campaign/%s' % (campaign_id)
 		output = api_call_post(self.application_key,self.user_api_key,endpoint,payload)
 		return output
 
 	'''payload = {"conversions":[{"conversion_reference_id":"abc123","status":"approved","reject_reason":""},{"conversion_reference_id":"def123","status":"rejected","reject_reason":"not an affiliate sale"}]}'''
 	def validate_transaction(self, payload,campaign_id):
-		endpoint = 'https://api.performancehorizon.com/campaign/%s/conversion' % (campaign_id)
+		endpoint = base_url + '/campaign/%s/conversion' % (campaign_id)
 		output = api_call_post(self.application_key,self.user_api_key,endpoint,payload)
 		return output
 
@@ -123,12 +123,12 @@ class Creative():
 
 	#payload = TBC
 	def create_creative(application_key,user_api_key,payload,campaign_id):
-		endpoint = 'https://api.performancehorizon.com/campaign/%s' % (campaign_id)
+		endpoint = base_url + '/campaign/%s' % (campaign_id)
 		output = api_call_post(self.application_key,self.user_api_key,endpoint,payload)
 		return output
 
 	def update_creative(application_key,user_api_key,payload,campaign_id):
-		endpoint = 'https://demoapi.performancehorizon.com/campaign/%s/creative' % (campaign_id)
+		endpoint = base_url + '/campaign/%s/creative' % (campaign_id)
 		output = api_call_put(self.application_key,self.user_api_key,endpoint,payload)
 		return output
 
